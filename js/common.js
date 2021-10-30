a6d123e9f795b975635a5cc5f94ca50eb779ee33
彭恒 <3010188620@qq.com>
2021-10-30 10:58:03 +0800

    'ph'

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 function setRem() {
     // 设计稿宽度和设备宽度
     var uiW = 375;
-    var winW = document.documentElement.clientWidth,
+    var winW = document.documentElement.clientWidth;
     // 定义比例
     var html_ = document.getElementsByTagName('html')[0];
 
@@ -10,3 +10,24 @@ function setRem() {
 // 调用函数
 setRem();
 window.onresize = setRem;
+
+// 导航栏
+var side = document.getElementsByClassName('side')[0];
+var line = document.getElementsByClassName('line')[0];
+var del = document.getElementsByClassName('del')[0];
+var ipt1 = document.getElementsByClassName('ipt1');
+var dob = document.getElementsByClassName('dob');
+touch.on(line, 'tap', function() {
+    side.style.display = 'block';
+    ipt1[0].style.display = 'block';
+})
+touch.on(del, 'tap', function() {
+    side.style.display = 'none';
+    dob[0].style.display = "none";
+})
+
+
+touch.on(ipt1[0], 'tap', function() {
+    ipt1[0].style.display = "none";
+    dob[0].style.display = "block";
+})
